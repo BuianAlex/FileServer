1f2ec9b22209bfda72d629bc4b9f1de9e517a655
Aleksandr <a.buyan@ukr.net>
2020-03-04 17:25:07 +0200

    add docker

--- db/conectDB.js
+++ db/conectDB.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { DB_HOSTNAME, DB_PORT, DB_NAME } = process.env;
+const url = `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`;
 
-mongoose.connect(process.env.DB_HOST, {
+mongoose.connect(url, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
-})
-mongoose.set('useCreateIndex', true)
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+});
+mongoose.set('useCreateIndex', true);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('db connected!')
-})
+  console.log('db connected!');
+});
 
-module.export = mongoose
+module.export = mongoose;
